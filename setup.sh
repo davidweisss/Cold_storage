afb19062c4826bbb315090459c0174fa7faef37e
David Weiss <davidweiss@dnavid.com>
2018-08-26 20:59:32 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 # Save Raspbian/Linux disk image from network connected rpi
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install git screen vim libhtml openssl pkgconf pkg install autoconf automake boost-libs libevent 
+sudo apt-get install autoconf libevent-dev libtool libssl-dev libboost-all-dev libminiupnpc-dev -y
