6a39337960aac64a15efdfdb9407796c34e0ad79
Your Name <you@example.com>
2018-08-26 19:57:01 +0000

    .

--- setup.sh
+++ setup.sh
@@ -6,5 +6,8 @@ git clone https://github.com/bitcoin/bitcoin.git
 sudo sed -i '16s/.*/CONF_SWAPSIZE=1024/g' /etc/dphys-swapfile
 sudo dphys-swapfile setup
 sudo dphys-swapfile swapon
-
+cd bitcoin
+./autogen.sh
+./configure
+sudo make install
 ## refs https://steemit.com/bitcoin/@mys/usd100-raspberry-pi2-bitcoin-light-node-with-tft-display-and-3d-printed-case
