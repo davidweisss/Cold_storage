975efc1e4aff5fecd01b551cc4f2480877feea13
David Weiss <davidweiss@dnavid.com>
2018-08-26 21:12:33 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,5 +6,10 @@ git clone https://github.com/bitcoin/bitcoin.git
 sudo sed -i '16s/.*/CONF_SWAPSIZE=1024/g' /etc/dphys-swapfile
 sudo dphys-swapfile setup
 sudo dphys-swapfile swapon
+cd bitcoin
+./autogen.sh
+./configure
+time make
+sudo make install
 
 ## refs https://steemit.com/bitcoin/@mys/usd100-raspberry-pi2-bitcoin-light-node-with-tft-display-and-3d-printed-case
