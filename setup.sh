ef1b7984632305ff6343a4802463aa76d7571248
David Weiss <davidweiss@dnavid.com>
2018-08-26 21:08:28 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 # Save Raspbian/Linux disk image from network connected rpi
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install autoconf libevent-dev libtool libssl-dev libboost-all-dev libminiupnpc-dev -y
+sudo apt-get install git vim screen autoconf libevent-dev libtool libssl-dev libboost-all-dev libminiupnpc-dev -y
+git clone https://github.com/bitcoin/bitcoin.git
+sudo sed -i '16s/.*/CONF_SWAPSIZE=1024/g' /etc/dphys-swapfile
+sudo dphys-swapfile setup
+sudo dphys-swapfile swapon
+
+## refs https://steemit.com/bitcoin/@mys/usd100-raspberry-pi2-bitcoin-light-node-with-tft-display-and-3d-printed-case
