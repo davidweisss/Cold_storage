e79dc366bf923d9169eaabe474bbd485765161de
David Weiss <davidweiss@dnavid.com>
2018-08-31 10:12:56 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -37,30 +37,7 @@ cd bitcoin
 time make
 sudo make install
 
-## Launching bitcoin daemon will create wallet.dat
-## Launch without internet connection, blocks are not synched [3]
-## Dev: screen
-## bitcoind -connect=0 -nolisten 
-bitcoind -connect=0 -nolisten -daemon
-##
 
-## Create new screen tab C^a-C^c
-## Dump wallet keys to file
-bitcoin-cli dumpwallet ~/.bitcoin/wallets/to-destroy-b4-going-online.txt 
-
-## Examine and note manually the relevant codes
-vim  ~/.bitcoin/wallets/to-destroy-b4-going-online.txt
-
-## Create new screen tab C^a-C^c
-## Remove new wallet and wallet dump, and all the data from bitcoin on ./bitcoin
-## Shred scrambles the data so that file cannot be recovered
-shred -zvu ~/.bitcoin
-
-## Do all cleanup necessary
-## If security critical, destroy the sd card physically.
-
-## Turn on wifi (for development, not for normal operation).
-## sudo ifconfig wlan0 up 
 
 ## [ 1 ] refs https://steemit.com/bitcoin/@mys/usd100-raspberry-pi2-bitcoin-light-node-with-tft-display-and-3d-printed-case
 ## [ 2 ] https://scribles.net/disabling-bluetooth-on-raspberry-pi/
