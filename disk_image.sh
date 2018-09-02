4a3776d59bffe2ac28cd1b0fb0a4841775192d66
David Weiss <davidweiss@dnavid.com>
2018-09-02 16:21:53 +0200

    Update disk_image.sh

--- disk_image.sh
+++ disk_image.sh
@@ -2,3 +2,5 @@
 
 ## Refs
 ## https://www.raspberrypi.org/documentation/installation/installing-images/linux.md
+## http://qdosmsq.dunbar-it.co.uk/blog/2013/06/noobs-for-raspberry-pi/
+## http://www.upubuntu.com/2012/01/how-to-clearerase-disk-partitions-from.html
