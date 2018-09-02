a31c79e3a39a734346768030ec80b9638bb6e0c2
David Weiss <davidweiss@dnavid.com>
2018-09-02 16:51:40 +0200

    Update disk_image.sh

--- disk_image.sh
+++ disk_image.sh
@@ -4,3 +4,4 @@
 ## https://www.raspberrypi.org/documentation/installation/installing-images/linux.md
 ## http://qdosmsq.dunbar-it.co.uk/blog/2013/06/noobs-for-raspberry-pi/
 ## http://www.upubuntu.com/2012/01/how-to-clearerase-disk-partitions-from.html
+## https://askubuntu.com/questions/153245/how-to-wipe-ram-on-shutdown-prevent-cold-boot-attacks
