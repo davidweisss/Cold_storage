ae8a6f8c7d35f7aaad0e76905ebe0d70e2af8b0f
David Weiss <davidweiss@dnavid.com>
2018-09-04 12:01:31 +0200

    Update importKeyAndSign.sh

--- importKeyAndSign.sh
+++ importKeyAndSign.sh
@@ -2,6 +2,10 @@
 bitcoin-cli createrawtransaction '[{"txid" : "9ca8f969bd3ef5ec2a8685660fdbf7a8bd365524c2e1fc66c309acbae2c14ae3", "vout" : 0}]''{"1LnfTndy3qzXGN19Jwscj1T8LR3MVe3JDb": 0.025, \
 "1hvzSofGwT8cjb8JU7nBsCSfEVQX5u9CL": 0.0245}' '{"1LnfTndy3qzXGN19Jwscj1T8LR3MVe3JDb": 0.025, "1hvzSofGwT8cjb8JU7nBsCSfEVQX5u9CL": 0.0245}'
 
+bitcoin-cli decoderawtransaction output_of_previous
+
+bitcoin-cli signrawtransaction
+
 ## Refs
 ## Antonopoulos, Mastering Bitcoin https://unglueit-files.s3.amazonaws.com/ebf/05db7df4f31840f0a873d6ea14dcc28d.pdf
 
