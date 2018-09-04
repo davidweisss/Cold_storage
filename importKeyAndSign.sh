4bf30c8fdad063906580605ffe19017c99ca70c3
David Weiss <davidweiss@dnavid.com>
2018-09-04 16:51:50 +0200

    Update importKeyAndSign.sh

--- importKeyAndSign.sh
+++ importKeyAndSign.sh
@@ -10,3 +10,4 @@ bitcoin-cli signrawtransaction
 ## Antonopoulos, Mastering Bitcoin https://unglueit-files.s3.amazonaws.com/ebf/05db7df4f31840f0a873d6ea14dcc28d.pdf
 
 ## https://medium.com/blockchain-education-network/how-to-write-stuff-on-the-blockchain-bdae1704f24d
+## https://github.com/bitcoin/bitcoin/issues/11546
