afd06d6efe88ef4df9654c4dd16d1bd41fa47e75
David Weiss <davidweiss@dnavid.com>
2018-09-13 11:48:10 +0200

    Update setupTransferAndBackupDrives.sh

--- setupTransferAndBackupDrives.sh
+++ setupTransferAndBackupDrives.sh
@@ -1,5 +1,10 @@
+## Insert 
 sudo fdisk /dev/sda
+sudo fdisk /dev/sdb
 ## Follow instructions https://thepihut.com/blogs/raspberry-pi-tutorials/17699796-formatting-and-mounting-a-usb-drive-from-a-terminal-window
 sudo mkfs -t vfat -I /dev/sda1
+sudo mkfs -t vfat -I /dev/sdb1
 mkdir /mnt/transfer
-sudo mount /dev/sda1 transfer -o umask=000
+mkdir /mnt/backup
+sudo mount /dev/sda1 /mnt/transfer -o umask=000
+sudo mount /dev/sdb1 /mnt/backup -o umask=000
