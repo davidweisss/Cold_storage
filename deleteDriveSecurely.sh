d169ebb1d6d3fddfdf4a669841bc10eac61d419a
David Weiss <davidweisss@gmail.com>
2018-09-16 23:43:58 +0200

    Update deleteDriveSecurely.sh

--- deleteDriveSecurely.sh
+++ deleteDriveSecurely.sh
@@ -11,3 +11,5 @@ sudo dd if=/dev/zero of=/dev/sda
 ## http://qdosmsq.dunbar-it.co.uk/blog/2013/06/noobs-for-raspberry-pi/
 ## https://askubuntu.com/questions/153245/how-to-wipe-ram-on-shutdown-prevent-cold-boot-attacks
 ## https://en.wikipedia.org/wiki/Cold_boot_attack
+## https://askubuntu.com/questions/703576/fdisk-l-shows-16-ram-disks-dev-ram0-ram15
+## --> "The RAM disk driver is a way to use main system memory as a block device. It is required for initrd, an initial filesystem used if you need to load modules in order to access the root filesystem (see Documentation/initrd.txt). It can also be used for a temporary filesystem for crypto work, since the contents are erased on reboot."
