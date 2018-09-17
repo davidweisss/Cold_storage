aeaedb8e9b1ced73100984a2d79d8e40a3c5a463
David Weiss <davidweisss@gmail.com>
2018-09-17 12:46:02 +0200

    Update deleteDriveSecurely.sh

--- deleteDriveSecurely.sh
+++ deleteDriveSecurely.sh
@@ -4,29 +4,35 @@
 
 ## List partition (check there is only one that takes all disk space...)
 sudo fdisk -l
-##Disk /dev/sda: 119,3 GiB, 128043712512 bytes, 250085376 sectors
-##Units: sectors of 1 * 512 = 512 bytes
-##Sector size (logical/physical): 512 bytes / 512 bytes
-##I/O size (minimum/optimal): 512 bytes / 512 bytes
-##Disklabel type: dos
-##Disk identifier: 0xa8560bcf
+## Disk /dev/sda: 119,3 GiB, 128043712512 bytes, 250085376 sectors
+## Units: sectors of 1 * 512 = 512 bytes
+## Sector size (logical/physical): 512 bytes / 512 bytes
+## I/O size (minimum/optimal): 512 bytes / 512 bytes
+## Disklabel type: dos
+## Disk identifier: 0xa8560bcf
 ##
-##Device     Boot Start       End   Sectors   Size Id Type
-##/dev/sda1        2048 250085375 250083328 119,3G 83 Linux
+## Device     Boot Start       End   Sectors   Size Id Type
+## /dev/sda1        2048 250085375 250083328 119,3G 83 Linux
 
 ## Find dev that gets added when you put in the card and flatten all its bytes to 0. Writes until an error occurs because there is no space left on the device. Can take long (eg full day) on large cards!
 sudo dd if=/dev/zero of=/dev/sda
-##dd: écriture vers '/dev/sda': Aucun espace disponible sur le périphérique
-##250085377+0 enregistrements lus
-##250085376+0 enregistrements écrits
-##128043712512 bytes (128 GB, 119 GiB) copied, 36658 s, 3,5 MB/s
+## dd: écriture vers '/dev/sda': Aucun espace disponible sur le périphérique
+## 250085377+0 enregistrements lus
+## 250085376+0 enregistrements écrits
+## 128043712512 bytes (128 GB, 119 GiB) copied, 36658 s, 3,5 MB/s
 
+## Download raspbian
 wget http://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2018-06-29/2018-06-27-raspbian-stretch-lite.zip
-jar xvf 2018-06-27-raspbian-stretch-lite.zip 
-
 ## Checksum of OS (raspbian)
 ## SHA-256:3271b244734286d99aeba8fa043b6634cad488d211583814a2018fc14
 ## https://www.raspberrypi.org/downloads/raspbian/
+
+## Verify download
+sha256sum 2018-06-27-raspbian-stretch-lite.zip 
+
+## Expand archive
+jar xvf 2018-06-27-raspbian-stretch-lite.zip 
+
 
 
 
