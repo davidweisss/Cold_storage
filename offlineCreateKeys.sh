13f3942ec297797493fb153507b1670765d1d8cf
David Weiss <davidweisss@gmail.com>
2018-09-18 09:28:49 +0200

    Update offlineCreateKeys.sh

--- offlineCreateKeys.sh
+++ offlineCreateKeys.sh
@@ -2,23 +2,19 @@
 ## Launch without internet connection, blocks are not synched [3]
 ## Dev: screen
 ## bitcoind -connect=0 -nolisten 
-bitcoind -connect=0 -nolisten -daemon
+bitcoind -connect=0 -nolisten -daemon 
 ##
 
 ## Create new screen tab C^a-C^c
-## Dump wallet keys to file
-bitcoin-cli dumpwallet ~/.bitcoin/wallets/to-destroy-b4-going-online.txt 
+newAddress=`bitcoin-cli getnewaddress`
+bitcoin-cli dumpprivkey $newAddress
 
-## Examine and note manually the relevant codes
-vim  ~/.bitcoin/wallets/to-destroy-b4-going-online.txt
+## Make a hard copy of the private key (optionally, the public key, which cam be recovered from the private key)
 
 ## Create new screen tab C^a-C^c
 ## Remove new wallet and wallet dump, and all the data from bitcoin on ./bitcoin
 ## Shred scrambles the data so that file cannot be recovered
 shred -zvu ~/.bitcoin
 
-## Do all cleanup necessary
-## If security critical, destroy the sd card physically.
-
-## Turn on wifi (for development, not for normal operation).
-## sudo ifconfig wlan0 up 
+## 
+## If security critical, destroy the sd card (+rpi for extreme security) physically.
