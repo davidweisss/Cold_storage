0a00fb584bc1b511e36e54adeeadf1e7e8a88560
David Weiss <davidweisss@gmail.com>
2018-10-24 20:07:07 +0000

    Read arguments from file, private key as argument

--- offlineTxCreateAndSign.sh
+++ offlineTxCreateAndSign.sh
@@ -1,15 +1,15 @@
-## Inputs input from full- internet-connected node
-data="616d7a6e2063617264"
-utxoAmount=0.1126
-txIdIn="433d6ff012b5e6a21189ada80ea2edad6a080e4a4bc7956c49ac1b034fafb7bc"
-addressIn="3Mt8B9LkZZaCoDueFVm5MEn8cbjKYjHPxQ"
-addressInPrivateKey=""
-outputNumberIn=1 ## (computable)
-payAmount=0.00391120
-addressOut="18S9sxPktsMkwuVHKAH8w3t3SMS5JcKhTo"
-## Miner fee computation
-txRateInSatPerByte=10
-txSize=269
+transfer=$2
+
+data=`cat $1|jq ".data"` 
+utxoAmount=`cat $1|jq ".utxoAmount"` 
+txIdIn=`cat $1|jq ".txIdIn"` 
+addressIn=`cat $1|jq ".addressIn"` 
+addressInPrivateKey="$3"
+outputNumberIn=`cat $1|jq ".outputNumberIn"` 
+payAmount=`cat $1|jq ".payAmount"` 
+addressOut=`cat $1|jq ".addressOut"` 
+txRateInSatPerByte=`cat $1|jq ".txRateInSatPerByte"` 
+txSize=`cat $1|jq ".txSize"` 
 
 satPerBitcoin=100000000 ## Def
 
@@ -20,16 +20,18 @@ minerFee=`echo 0$minerFee|sed 's/0*$//g'`
 changeAmount=`echo $utxoAmount' - '$payAmount' - '$minerFee|bc -l`
 changeAmount=`echo 0$changeAmount|sed 's/0*$//g'`
 
-## Create tx
-rawTxUnsigned=`bitcoin-cli createrawtransaction '[{"txid": "'$txIdIn'","vout": '$outputNumberIn'}]' '{"'$addressOut'": '$payAmount',"'$addressIn'": '$changeAmount',"data": "'$data'"}'`
+## Create tx     
+rawTxUnsigned=`bitcoin-cli createrawtransaction '[{"txid":'$txIdIn',"vout":'$outputNumberIn'}]' '{'$addressOut':'$payAmount','$addressIn':'$changeAmount',"data":'$data'}'`
 
 rawTxUnsignedDecoded=`bitcoin-cli decoderawtransaction $rawTxUnsigned`
 scriptHashOut=`echo $rawTxUnsignedDecoded|jq -r '.vout|.[0].scriptPubKey.hex'`
 scriptHashIn=`echo $rawTxUnsignedDecoded|jq -r '.vout|.[1].scriptPubKey.hex'`
 
 ## OFFLINE: Sign tx
-rawTxSigned=`bitcoin-cli signrawtransactionwithkey $rawTxUnsigned '["'$addressInPrivateKey'"]' '[{"txid": "'$txIdIn'" ,"vout": '$outputNumberIn', "scriptPubKey": "'$scriptHashIn'", "redeemScript": "'$scriptHashOut'", "amount": '$utxoAmount'}]'`
+rawTxSigned=`bitcoin-cli signrawtransactionwithkey $rawTxUnsigned '["'$addressInPrivateKey'"]' '[{"txid": '$txIdIn' ,"vout": '$outputNumberIn', "scriptPubKey": "'$scriptHashIn'", "redeemScript": "'$scriptHashOut'", "amount": '$utxoAmount'}]'`
 rawTxSigned=`echo $rawTxSigned| jq -r '.hex'`
+echo Writing raw signed transaction to $2
 
 echo $rawTxSigned >> $transfer
-
+echo `date`>> $transfer
+cat $1 >> $transfer
